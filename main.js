d4bce12a42790e31456a3877fdea4a75ead77097
ayyamperumal <81428707+ayyamperumal@users.noreply.github.com>
2022-02-11 19:00:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Status="";
+function preload(){
+    img=loadImage('bottle.jpg');
+}
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+}
